b7b10019fbdbe53c1f1436be79e1601549fda9c3
Deserty0 <86846189+Deserty0@users.noreply.github.com>
2026-05-28 22:24:07 +1000

    win-win

---
+++ Content.Shared/_CE/StatusEffects/Core/Components/CEStatusEffectAlertOnStackComponent.cs
@@ -0,0 +1,24 @@
+﻿using Content.Shared.Alert;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._CE.StatusEffects.Core.Components;
+
+/// <summary>
+/// Changes alert on stack
+/// </summary>
+[RegisterComponent]
+public sealed partial class CEStatusEffectAlertOnStackComponent : Component
+{
+    /// <summary>
+    /// Category of alerts
+    /// </summary>
+    [DataField(required: true)]
+    public ProtoId<AlertCategoryPrototype> AlertsCategory;
+
+    /// <summary>
+    /// Stack: Alert
+    /// Alerts must be in same group
+    /// </summary>
+    [DataField(required: true)]
+    public SortedDictionary<int, ProtoId<AlertPrototype>>  Alerts;
+}
