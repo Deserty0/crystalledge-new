1559e8d56f569b19514114e8f76b7f95917eff9b
Deserty0 <86846189+Deserty0@users.noreply.github.com>
2026-05-31 17:06:57 +1000

    You showed up, and had my doors unlocked

---
+++ Content.Shared/_CE/StatusEffects/ChangeMaxHp/CEChangeMaxHpStatusEffectComponent.cs
@@ -0,0 +1,34 @@
+﻿namespace Content.Shared._CE.StatusEffects.ChangeMaxHp;
+
+/// <summary>
+/// This is used for changing target hp
+/// Positive delta is adding hp
+/// Negative delta is reducing hp
+/// </summary>
+[RegisterComponent]
+public sealed partial class CEChangeMaxHpStatusEffectComponent : Component
+{
+    /// <summary>
+    /// Changes max HP by flat amount
+    /// </summary>
+    [DataField]
+    public int FlatHpChange = 0;
+
+    /// <summary>
+    /// Changes max HP by flat amount per stack
+    /// </summary>
+    [DataField]
+    public int FlatHpChangePerStack = 0;
+
+    /// <summary>
+    /// Changes max HP by percent
+    /// </summary>
+    [DataField]
+    public float HpMultiplierChange = 0;
+
+    /// <summary>
+    /// Changes max HP by percent per stack
+    /// </summary>
+    [DataField]
+    public float HpMultiplierChangePerStack = 0;
+}
---
+++ Content.Shared/_CE/StatusEffects/ChangeMaxHp/CEChangeMaxHpStatusEffectSystem.cs
@@ -0,0 +1,47 @@
+﻿using Content.Shared._CE.Health;
+using Content.Shared._CE.StatusEffects.Core;
+using Content.Shared._CE.StatusEffects.Core.Components;
+using Content.Shared.StatusEffectNew;
+
+namespace Content.Shared._CE.StatusEffects.ChangeMaxHp;
+
+public sealed class CEChangeMaxHpStatusEffectSystem : EntitySystem
+{
+    [Dependency] private readonly CEMobStateSystem _mobState = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<CEChangeMaxHpStatusEffectComponent, StatusEffectAppliedEvent>(OnApply);
+        SubscribeLocalEvent<CEChangeMaxHpStatusEffectComponent, StatusEffectRemovedEvent>(OnRemoved);
+        SubscribeLocalEvent<CEChangeMaxHpStatusEffectComponent, CEStatusEffectStackEditedEvent>(OnStackEdited);
+        SubscribeLocalEvent<CEChangeMaxHpStatusEffectComponent, StatusEffectRelayedEvent<CECalculateMaxHealthEvent>>(OnCalculateMaxHealth);
+    }
+
+    private void OnApply(Entity<CEChangeMaxHpStatusEffectComponent> ent, ref StatusEffectAppliedEvent args)
+    {
+        _mobState.RefreshMaxHealth(args.Target);
+    }
+
+    private void OnRemoved(Entity<CEChangeMaxHpStatusEffectComponent> ent, ref StatusEffectRemovedEvent args)
+    {
+        _mobState.RefreshMaxHealth(args.Target);
+    }
+
+    private void OnStackEdited(Entity<CEChangeMaxHpStatusEffectComponent> ent, ref CEStatusEffectStackEditedEvent args)
+    {
+        _mobState.RefreshMaxHealth(args.Target);
+    }
+
+    private void OnCalculateMaxHealth(Entity<CEChangeMaxHpStatusEffectComponent> ent,
+        ref StatusEffectRelayedEvent<CECalculateMaxHealthEvent> args)
+    {
+        var stacks = 1;
+        if (TryComp<CEStatusEffectStackComponent>(ent, out var stackComp))
+            stacks = stackComp.Stacks;
+
+        args.Args.FlatModifier += ent.Comp.FlatHpChange + ent.Comp.FlatHpChangePerStack * stacks;
+        args.Args.Multiplier += ent.Comp.HpMultiplierChange + ent.Comp.HpMultiplierChangePerStack * stacks;
+    }
+}
