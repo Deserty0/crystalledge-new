739f665b9df874e2d35dd55fbc010f0596665bfb
Deserty0 <86846189+Deserty0@users.noreply.github.com>
2026-05-31 23:46:28 +1000

    We are built to overcome endless mishaps

---
+++ Content.Shared/_CE/StatusEffects/SeveredMemories/CEStatusEffectSeveredMemoriesComponent.cs
@@ -0,0 +1,52 @@
+﻿using Content.Shared._CE.Health;
+using Content.Shared._CE.Health.Prototypes;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._CE.StatusEffects.SeveredMemories;
+
+[RegisterComponent]
+public sealed partial class CEStatusEffectSeveredMemoriesComponent : Component
+{
+    /// <summary>
+    /// How many stacks is needed to start loosing random abilities
+    /// </summary>
+    [DataField]
+    public int AbilityLoosingThreshold = 20;
+
+    /// <summary>
+    /// Loose random ability each X stack
+    /// </summary>
+    [DataField]
+    public int AbilityLoosingStack = 10;
+
+    public int? LastLoosedAbilityStack;
+
+    /// <summary>
+    /// How many stacks is needed to start fading away
+    /// </summary>
+    [DataField]
+    public int FadingThreshold = 60;
+
+    /// <summary>
+    /// How many stacks is needed to start dying
+    /// </summary>
+    [DataField]
+    public int DyingThreshold = 90;
+
+    /// <summary>
+    /// How many hp is loosed per time
+    /// </summary>
+    [DataField]
+    public int HpLoose = 1;
+
+    [DataField(required: true)]
+    public ProtoId<CEDamageTypePrototype> DamageType;
+
+    /// <summary>
+    /// How often hp loose is occuring
+    /// </summary>
+    [DataField]
+    public TimeSpan HpLooseCycle = TimeSpan.FromSeconds(1);
+
+    public TimeSpan NextHpLoose = TimeSpan.Zero;
+}
---
+++ Content.Shared/_CE/StatusEffects/SeveredMemories/CEStatusEffectSeveredMemoriesSystem.cs
@@ -0,0 +1,92 @@
+﻿using Content.Shared._CE.Health;
+using Content.Shared._CE.Health.Components;
+using Content.Shared._CE.Skill.Core;
+using Content.Shared._CE.Skill.Core.Components;
+using Content.Shared._CE.StatusEffects.Core;
+using Content.Shared._CE.StatusEffects.Core.Components;
+using Content.Shared.Fluids;
+using Content.Shared.StatusEffectNew.Components;
+using Robust.Shared.Random;
+using Robust.Shared.Timing;
+
+namespace Content.Shared._CE.StatusEffects.SeveredMemories;
+
+public sealed class CEStatusEffectSeveredMemoriesSystem : EntitySystem
+{
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly CESharedDamageableSystem _damage = default!;
+    [Dependency] private readonly CESharedSkillSystem _skills = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<CEStatusEffectSeveredMemoriesComponent, CEStatusEffectStackEditedEvent>(OnStackEdited);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQuery<CEStatusEffectSeveredMemoriesComponent, CEStatusEffectStackComponent, StatusEffectComponent>();
+
+        foreach (var (comp, stack, status) in query)
+        {
+            if (comp.DyingThreshold > stack.Stacks)
+                continue;
+
+            if (comp.NextHpLoose > _timing.CurTime)
+                continue;
+
+            if (status.AppliedTo is null)
+                continue;
+
+            var damage = new CEDamageSpecifier(comp.DamageType, comp.HpLoose);
+            _damage.TakeDamage(status.AppliedTo.Value, damage);
+        }
+    }
+
+    private void OnStackEdited(Entity<CEStatusEffectSeveredMemoriesComponent> ent,
+        ref CEStatusEffectStackEditedEvent args)
+    {
+        LooseSkills(ent, args.NewStack, args.Target);
+
+    }
+
+    private void LooseSkills(Entity<CEStatusEffectSeveredMemoriesComponent> ent, int stack, EntityUid target)
+    {
+        if (ent.Comp.AbilityLoosingThreshold > stack)
+            return;
+
+        if (ent.Comp.LastLoosedAbilityStack is null)
+        {
+            ent.Comp.LastLoosedAbilityStack = stack;
+            UnlearnRandomSkill(target);
+            return;
+        }
+
+        var stackDelta = stack - ent.Comp.LastLoosedAbilityStack.Value;
+
+        if (stackDelta <= ent.Comp.AbilityLoosingThreshold)
+            return;
+
+        for (var i = 0; i < stackDelta / ent.Comp.AbilityLoosingThreshold; i++)
+        {
+            UnlearnRandomSkill(target);
+        }
+    }
+
+    private void UnlearnRandomSkill(EntityUid target)
+    {
+        if (!TryComp<CESkillStorageComponent>(target, out var storage))
+            return;
+
+        if (storage.LearnedSkills.Count == 0)
+            return;
+
+        var rand = new RobustRandom();
+        rand.SetSeed((int)_timing.CurTick.Value);
+
+        _skills.TryRemoveSkill(target, rand.Pick(storage.LearnedSkills), storage);
+    }
+}
